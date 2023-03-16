da20da2b55c1fc71826b697c7e57e658c209036e
andrew clark <andy-clark316@hotmail.com>
2023-03-16 15:19:36 +1100

    updated js file for dayjs format

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,109 +1,84 @@
 var dayjs= dayjs();
 var date= dayjs.format("DD/MM/YYYY h:mm:ss A");
-var dateHour= dayjs.format("h");
+var dateHour= dayjs.format('h');
+var dateAMPM= dayjs.format("A");
 $("#currentDay").text(date);
+var dateHourNum = Number(dateHour)
+var hourampm= dateHour+dateAMPM;
 
 for(var i=1; i < 25; i++){
-  if(i < 12){
-    $(".dayz").append('<div id="hour-'+i+'" class="row time-block past"></div>');
-    $("#hour-"+i).append(
-      '<div id="hour-'+i+'" class="row time-block past"></div><div class="col-2 col-md-1 hour text-center py-3">'+i+':00 AM</div><textarea class="col-8 col-md-10 description-'+i+'" rows="3"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i> </button></div>'
-      );
-  }else{
-    $(".dayz").append('<div id="hour-'+i+'" class="row time-block past"></div>');
-    $("#hour-"+i).append(
-      '<div class="col-2 col-md-1 hour text-center py-3">'+i+':00 PM</div><textarea class="col-8 col-md-10 description-'+i+'" rows="3"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i> </button></div>'
-      );
+    if(i < 13){
+      $(".dayz").append('<div id="hour-'+i+'" class="row time-block '+i+'AM"></div>');
+      $("#hour-"+i).append(
+        '<div id="hour-'+i+'" class="row time-block past"></div><div class="col-2 col-md-1 hour text-center py-3">'+i+':00 AM</div><textarea class="col-8 col-md-10 description-'+i+'" rows="3"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i> </button></div>'
+        );
+    }else{
+      $(".dayz").append('<div id="hour-'+i+'" class="row time-block '+i+'PM"></div>');
+      $("#hour-"+i).append(
+        '<div class="col-2 col-md-1 hour text-center py-3">'+i+':00 PM</div><textarea class="col-8 col-md-10 description-'+i+'" rows="3"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i> </button></div>'
+        );
+    }
+}
+dateHourNum= 6;
+dateAMPM= "PM";
+
+
+if(dateAMPM == "PM"){
+  for(var i =1; i < 13; i++){
+    if( i == dateHourNum){
+      var lab1= i + 12;
+      var lab= lab1+'PM';
+      $("."+lab).css("background", "#fc8f8d");
+
+    }else if(i < dateHourNum){
+      var lab1= i + 12;
+      var lab= lab1+'PM';
+      $("."+lab).css("background", "#bfbbbc");
+    }else if(i >dateHourNum){
+      var lab1= i + 12;
+      var lab= lab1+'PM';
+      $("."+lab).css("background", "#4adb37");
+    }
+    var lab= i+'AM';
+    $("."+lab).css("background", "#bfbbbc");
   }
+
+}else if(dateAMPM == "AM"){
+  for(var i =1; i < 13; i++){
+    if( i == dateHourNum){
+      var lab= i+'AM';
+      $("."+lab).css("background", "#fc8f8d");
+      
+    }else if(i < dateHourNum){
+      var lab= i+'AM';
+      $("."+lab).css("background", "#bfbbbc");
+      
+    }else if(i >dateHourNum){
+      var lab= i+'AM';
+      
+      $("."+lab).css("background", "#4adb37");
+    }
+    
+    var lab1= i + 12;
+    var lab= lab1+'PM';
+    $("."+lab).css("background", "#4adb37");
+  }
+
 }
 var saveBtn= $(".saveBtn");
 
 function getLocal(){
 
-for(var i= 0; i < 25; i ++){
+  for(var i= 0; i < 25; i ++){
   var test = localStorage.getItem('hour-'+i);
-  
-  if(i == 1){
     $(".description-"+i).val(test);
   }
-  else if(i == 2){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 3){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 4){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 5){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 6){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 7){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 8){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 9){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 10){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 11){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 12){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 13){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 14){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 15){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 16){
-    $(".description-"+i).val(test); 
-  }
-  else if(i == 17){
-    $(".description-"+i).val(test); 
-  }
-  else if(i == 18){
-    $(".description-"+i).val(test); 
-  }
-  else if(i == 19){
-    $(".description-"+i).val(test);
-  }
-  else if(i == 20){
-    $(".description-"+i).val(test);  
-  }
-  else if(i == 21){
-    $(".description-"+i).val(test);   
-  }
-  else if(i == 22){
-    $(".description-"+i).val(test);   
-  }
-  else if(i == 23){
-    $(".description-"+i).val(test); 
-  }
-  else if(i == 24){
-    $(".description-"+i).val(test); 
-  }
-}
-
 }
 $(saveBtn.click(function (){
   var saveBtnId= $(this).parent("div").attr("id");
   var saveBtnclass= $(this).parent("div").attr("class");
   var saveBtnTextArea= $(this).parent("div").children("textarea").val();
   localStorage.setItem(saveBtnId , saveBtnTextArea); 
-}));
-getLocal()
-  
 
+}));
+getLocal();
